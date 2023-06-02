53abee14d3071ece766372c174b5c5eded9a3c1a
Jzzr1986 <93964289+Jzzr1986@users.noreply.github.com>
2023-06-02 17:39:19 +0800

    Update FirstExam.java

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,5 +1,6 @@
 public class FirstExam {
     public  static void main(String[] args) {
      System.out.println("This is a test \n ");
+           System.out.println("This is a test in github \n ");
      }
 }
