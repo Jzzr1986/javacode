b0291fb30ad4a965264b6cbf8a5e07822ff9790c
Jzzr1986 <jzzr1986@163.com>
2023-06-02 18:55:52 +0800

    this is a test 2

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,6 +1,10 @@
 public class FirstExam {
     public  static void main(String[] args) {
      System.out.println("This is a test \n ");
-           System.out.println("This is a test in github \n ");
+     System.out.println("This is a test in github \n ");
+        System.out.println("This is a test in idea \n ");
+        int  i=1 ;
+        System.out.println("This is a test in idea "+ i + "\n");
+        i++;
      }
 }
